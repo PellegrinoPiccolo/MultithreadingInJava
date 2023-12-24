8305b986d13a5725dc942db1af7a42ee7aff72d3
PellegrinoPiccolo <152791429+PellegrinoPiccolo@users.noreply.github.com>
2023-12-24 19:13:38 +0100

    General changes

--- src/SynchronizedWithLock.java
+++ src/SynchronizedWithLock.java
@@ -1,16 +1,39 @@
+/**
+ * The type Synchronized with lock.
+ */
 public class SynchronizedWithLock implements Runnable{
+    /**
+     * The constant value.
+     */
     private static int value = 50;
+    /**
+     * The constant lock.
+     */
     private static Object lock = new Object();
+
+    /**
+     * Get.
+     */
     public void get(){
         synchronized (lock){
             value -= 5;
             System.out.println("Subtracted value " + value + " from: " + Thread.currentThread().getName());
         }
     }
+
+    /**
+     * Override Run method.
+     */
     @Override
     public void run() {
         get();
     }
+
+    /**
+     * Main where execute threads.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         Thread t1 = new Thread(new SynchronizedWithLock(), "T1");
         Thread t2 = new Thread(new SynchronizedWithLock(), "T2");
